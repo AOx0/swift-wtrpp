f3378748c2f07f7c1f79bff0f2f056eed6541866
AOx0 <alexlopez302b@gmail.com>
2020-06-22 04:43:29 -0500

    Initial

---
+++ swift-wtrpp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-wtrpp
+//
+//  Created by Alejandro D on 22/06/20.
+//  Copyright © 2020 Alejandro D. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
